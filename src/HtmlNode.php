6ac9cd447f2b66c655fd17f0ef06db7e3668f43d
Martin <hello@northrook.com>
2024-09-08 16:15:09 +0100

    mend

--- src/HtmlNode.php
+++ src/HtmlNode.php
@@ -4,8 +4,12 @@ declare( strict_types = 1 );
 
 namespace Northrook\HTML;
 
+use JetBrains\PhpStorm\Language;
+use Northrook\Logger\Log;
 use Northrook\Trait\PropertyAccessor;
 use Northrook\Minify;
+use const Northrook\EMPTY_STRING;
+
 
 /**
  * @property-read string html
@@ -16,18 +20,21 @@ readonly class HtmlNode
 {
     use PropertyAccessor;
 
+
     public \DOMDocument $dom;
 
     public function __construct(
         ?string $html = null,
-    ) {
+    )
+    {
         $this->dom = new \DOMDocument();
         if ( $html ) {
-            $this->loadHtml( $html );
+            $this->load( $html );
         }
     }
 
-    public function __get( string $property ) {
+    public function __get( string $property )
+    {
         return match ( $property ) {
             'html'       => $this->getHtml(),
             'innerHtml'  => $this->getInnerHtml(),
@@ -35,16 +42,48 @@ readonly class HtmlNode
         };
     }
 
-    public function loadHtml( string $string ) : void {
-        $html = Minify::HTML( $string, false )->toString();
-        $this->dom->loadHTML( $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOERROR );
+    final public function load(
+        #[Language( 'HTML' )]
+        string $html,
+    ) : self
+    {
+        try {
+            $this->dom->loadHTML(
+                source  : '<div>' . \str_replace( "\r\n", "\n", $html ) . '</div>',
+                options : LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD,
+            );
+            $this->dom->encoding = 'UTF-8';
+        }
+        catch ( \Exception $exception ) {
+            $this->errorHandler( $exception );
+        }
+
+        return $this;
     }
 
-    protected function getHtml() : string {
-        return $this->dom->saveHTML();
+    // public function loadHtml( string $string ) : void
+    // {
+    //     $html = Minify::HTML( $string, false )->toString();
+    //     $this->dom->loadHTML( $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOERROR );
+    // }
+
+    protected function getHtml() : string
+    {
+        $content = EMPTY_STRING;
+
+        foreach ( $this->getChildNodes() as $node ) {
+            $content .= $this->dom->saveXML( $node, options : LIBXML_NOXMLDECL );
+        }
+
+        // foreach ( Tag::SELF_CLOSING as $tag ) {
+        //     $content = \str_replace( "<$tag></$tag>", "<$tag/>", $content );
+        // }
+        return $content;
     }
 
-    protected function getInnerHtml() : string {
+    protected function getInnerHtml() : string
+    {
+        trigger_error( 'Deprecated', E_USER_DEPRECATED );
         $html = '';
         foreach ( $this->getChildNodes() as $childNode ) {
             $html .= $childNode->ownerDocument->saveHTML( $childNode );
@@ -55,12 +94,21 @@ readonly class HtmlNode
     /**
      * @return \DOMElement[]
      */
-    public function getChildNodes() : array {
-        return \iterator_to_array( $this->dom->firstElementChild->childNodes );
+    public function getChildNodes() : array
+    {
+        return \iterator_to_array( $this->dom->documentElement?->childNodes ?? [] );
     }
 
-    protected function getAttributes() : array {
+    /**
+     * @return \DOMNodeList|iterable
+     */
+    public function iterateChildNodes() : \DOMNodeList | iterable
+    {
+        return $this->dom->documentElement?->childNodes ?? [];
+    }
 
+    protected function getAttributes() : array
+    {
         $attributes = [];
 
         $node = $this->dom->firstElementChild;
@@ -76,8 +124,8 @@ readonly class HtmlNode
         return $attributes;
     }
 
-    public static function extractAttributes( string $html ) : array {
-
+    public static function extractAttributes( string $html ) : array
+    {
         $html = Minify::HTML( $html, false )->toString();
 
         if ( false === \str_starts_with( $html, '<' )
@@ -95,5 +143,43 @@ readonly class HtmlNode
         }
 
         return ( new HtmlNode( $html ) )->getAttributes();
+    }
+
+    public static function unwrap( string $html, string ...$tags ) : string
+    {
+        $element = new static( $html );
+
+        foreach ( $element->iterateChildNodes() as $childNode ) {
+            if ( !\in_array( $childNode->nodeName, $tags ) ) {
+                continue;
+            };
+
+            foreach ( $childNode->childNodes as $nestedChild ) {
+                $childNode->parentNode->insertBefore( $nestedChild->cloneNode( true ), $childNode );
+            }
+            $childNode->parentNode->removeChild( $childNode );
+        }
+
+        return $element->getHtml();
+    }
+
+    private function errorHandler( \Exception $exception ) : void
+    {
+        if ( $exception instanceof \ErrorException ) {
+            $severity = $exception->getSeverity();
+            $message  = $exception->getMessage();
+
+            if ( \str_contains( $message, ' invalid in Entity, ' ) ) {
+                return;
+            }
+
+            //: We will likely downright skip all down the line
+            // if ( $severity === E_WARNING ) {
+            //     return;
+            // }
+        }
+
+        dump( $exception );
+        Log::exception( $exception );
     }
 }
