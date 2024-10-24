9b3c11df0fd17b0ccaf3d48d598ac4cc43b09a4a
Martin <hello@northrook.com>
2024-10-24 12:50:50 +0100

    mend

--- src/Format.php
+++ src/Format.php
@@ -21,40 +21,37 @@ class Format
     private array $textStringCallbacks = [];
 
     public static function title(
-            string  $string,
-            Purpose $type,
-    ) : string
-    {
-        $string = trim( $string );
+        string  $string,
+        Purpose $type,
+    ) : string {
+        $string = trim($string);
 
-        $string = match ( $type ) {
-            Purpose::Title     => ucfirst( $string ), // Limit to `<title>`
-            Purpose::Paragraph => ucfirst( $string ), // Limit to recommended paragraph length
+        $string = match ($type) {
+            Purpose::Title     => ucfirst($string), // Limit to `<title>`
+            Purpose::Paragraph => ucfirst($string), // Limit to recommended paragraph length
             default            => $string
         };
 
-        return escapeHtml( $string );
+        return escapeHtml($string);
     }
 
-    public static function nl2p( string $string ) : string
+    public static function nl2p(string $string) : string
     {
-        return Format::newline( $string, Newline::Paragraph );
+        return Format::newline($string, Newline::Paragraph);
     }
 
-    public static function nl2s( string $string ) : string
+    public static function nl2s(string $string) : string
     {
-        return Format::newline( $string, Newline::Span );
+        return Format::newline($string, Newline::Span);
     }
 
-    public static function inline( string $string ) : string
+    public static function inline(string $string) : string
     {
-        if ( !$string ) {
+        if (!$string) {
             return EMPTY_STRING;
         }
 
-        $html = Minify::HTML( $string );
-
-        $html = new HtmlTextFormatter( $html );
+        $html = new HtmlTextFormatter((string)  new Minify\HtmlMinifier($string));
 
         return $html
                 ->wrapTrailingWords()
@@ -63,28 +60,27 @@ class Format
     }
 
     public static function newline(
-            string  $string,
-            Newline $strategy = Newline::Auto,
-            array   $attributes = [],
-    ) : string
-    {
+        string  $string,
+        Newline $strategy = Newline::Auto,
+        array   $attributes = [],
+    ) : string {
         // Trim the provided string.
-        $string = \trim( $string );
+        $string = \trim($string);
 
         // Bail early if the string was empty, null, or nothing but whitespace.
-        if ( !$string ) {
+        if (!$string) {
             return EMPTY_STRING;
         }
 
         // Explode the string into lines.
-        $lines = Format::explodeLinebreaks( $string );
+        $lines = Format::explodeLinebreaks($string);
 
-        return match ( $strategy ) {
-            Newline::Paragraph => Format::implodeWrap( $lines, 'p', $attributes ),
-            Newline::Span      => Format::implodeWrap( $lines, 'span', $attributes ),
-            Newline::Auto      => \count( $lines ) === 1
-                    ? Format::implodeWrap( $lines, 'span', $attributes )
-                    : Format::implodeWrap( $lines, 'p', $attributes ),
+        return match ($strategy) {
+            Newline::Paragraph => Format::implodeWrap($lines, 'p', $attributes),
+            Newline::Span      => Format::implodeWrap($lines, 'span', $attributes),
+            Newline::Auto      => \count($lines) === 1
+                    ? Format::implodeWrap($lines, 'span', $attributes)
+                    : Format::implodeWrap($lines, 'p', $attributes),
         };
     }
 
@@ -94,19 +90,19 @@ class Format
      *
      * @return string
      */
-    public static function backtickCodeTags( string $string, array $attributes = [] ) : string
+    public static function backtickCodeTags(string $string, array $attributes = []) : string
     {
         // TODO: Code highlighter | Use tempest/highlight
         return \preg_replace_callback(
-                '/`(\S.*?)`/m',
-                static fn( $matches ) => new Element( 'code', $attributes, escapeHtml( $matches[ 1 ] ) ),
-                $string,
+            '/`(\S.*?)`/m',
+            static fn ($matches) => new Element('code', $attributes, escapeHtml($matches[ 1 ])),
+            $string,
         );
     }
 
-    public static function textContent( string $string ) : string
+    public static function textContent(string $string) : string
     {
-        $html = new HtmlTextFormatter( $string );
+        $html = new HtmlTextFormatter($string);
 
         return $html
                 ->wrapTrailingWords()
@@ -114,38 +110,38 @@ class Format
                 ->toString();
     }
 
-    private static function hyphenLineBreakOpportunity( string $string, bool | int &$count = 0 ) : string
+    private static function hyphenLineBreakOpportunity(string $string, bool | int &$count = 0) : string
     {
-        return \preg_replace( '#\w(-)\w#m', '<wbr/>$1', $string, count : $count );
+        return \preg_replace('#\w(-)\w#m', '<wbr/>$1', $string, count : $count);
     }
 
-    public static function whiteSpaceWrap( string $string, ?int $maxTail = null ) : string
+    public static function whiteSpaceWrap(string $string, ?int $maxTail = null) : string
     {
         $maxTail ??= 12;
 
-        if ( \strlen( $string ) < $maxTail ) {
-            return '<span style="white-space: nowrap">' . \trim( $string ) . '</span>';
+        if (\strlen($string) < $maxTail) {
+            return '<span style="white-space: nowrap">' . \trim($string) . '</span>';
         }
 
-        $exploded = \array_reverse( \explode( ' ', $string ) );
+        $exploded = \array_reverse(\explode(' ', $string));
         $pieces   = [];
         $length   = 0;
 
-        foreach ( $exploded as $index => $pieceString ) {
-            $length   += \strlen( $pieceString );
+        foreach ($exploded as $index => $pieceString) {
+            $length   += \strlen($pieceString);
             $pieces[] = $pieceString;
-            unset( $exploded[ $index ] );
+            unset($exploded[ $index ]);
 
-            if ( $length >= $maxTail ) {
+            if ($length >= $maxTail) {
                 break;
             }
         }
 
-        $exploded = \array_reverse( $exploded );
-        $pieces   = \array_reverse( $pieces );
+        $exploded = \array_reverse($exploded);
+        $pieces   = \array_reverse($pieces);
 
-        $string = \implode( ' ', $exploded )
-                  . ' <span class="inline-runt">' . \trim( \implode( ' ', $pieces ) ) . '</span>';
+        $string = \implode(' ', $exploded)
+                  . ' <span class="inline-runt">' . \trim(\implode(' ', $pieces)) . '</span>';
 
         return $string;
     }
